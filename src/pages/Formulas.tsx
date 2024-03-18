a5fa484fa524adaac5dd1677a3b5af6319dcd8b7
bedada <bedada6@gmail.com>
2024-03-18 19:28:31 +0300

    responsive update

--- src/pages/Formulas.tsx
+++ src/pages/Formulas.tsx
@@ -4,14 +4,14 @@ import FormulaOutputs from "./FormulaOutputs";
 
 const Formulas = () => {
   return (
-    <div className="flex  gap-0 bg-[#F6F8FA] pt-10 px-4 w-full h-[100%] overflow-hidden">
-      <div className="w-[30%] bg-[#FFFFFF] border h-auto py-4">
+    <div className="flex flex-col lg:flex-row  gap-0 bg-[#F6F8FA] pt-10 px-4 w-full h-[100%] overflow-hidden">
+      <div className="w-full lg:w-[30%] bg-[#FFFFFF] border h-auto py-4">
         <FormulaInputs />
       </div>
-      <div className="w-[40%]  bg-[#FFFFFF]  border h-auto py-4 overflow-auto">
+      <div className="w-full lg:w-[40%]  bg-[#FFFFFF]  border h-auto py-4 overflow-auto">
         <FormulBody />
       </div>
-      <div className="w-[30%]  bg-[#FFFFFF]  border h-auto py-4">
+      <div className="w-full lg:w-[30%]  bg-[#FFFFFF]  border h-auto py-4">
         <FormulaOutputs />
       </div>
     </div>
