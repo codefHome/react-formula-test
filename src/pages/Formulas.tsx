8097b88c43138387822cdc96faa9c5b1b1db23c7
bedada <bedada6@gmail.com>
2024-03-18 19:26:09 +0300

    layout issue fixed

--- src/pages/Formulas.tsx
+++ src/pages/Formulas.tsx
@@ -1,23 +1,21 @@
-
-import FormulBody from './FormulBody'
-import FormulaInputs from './FormulaInputs'
-import FormulaOutputs from './FormulaOutputs'
-
-
+import FormulBody from "./FormulBody";
+import FormulaInputs from "./FormulaInputs";
+import FormulaOutputs from "./FormulaOutputs";
 
 const Formulas = () => {
   return (
-    <div className="grid grid-rows-12 grid-flow-col gap-0 bg-[#F6F8FA] pt-10 px-4 w-full h-[100%] overflow-hidden">
-        <div className="col-span-2 bg-[#FFFFFF] border h-full py-4"
-        >
-           <FormulaInputs/>
-           </div>
-           <div className='col-span-8  bg-[#FFFFFF]  border h-full py-4 overflow-auto'><FormulBody/></div>
-           <div className="col-span-2  bg-[#FFFFFF]  border h-full py-4"><FormulaOutputs/></div>
-      
-       
-         </div>
-  )
-}
+    <div className="flex  gap-0 bg-[#F6F8FA] pt-10 px-4 w-full h-[100%] overflow-hidden">
+      <div className="w-[30%] bg-[#FFFFFF] border h-auto py-4">
+        <FormulaInputs />
+      </div>
+      <div className="w-[40%]  bg-[#FFFFFF]  border h-auto py-4 overflow-auto">
+        <FormulBody />
+      </div>
+      <div className="w-[30%]  bg-[#FFFFFF]  border h-auto py-4">
+        <FormulaOutputs />
+      </div>
+    </div>
+  );
+};
 
-export default Formulas
+export default Formulas;
